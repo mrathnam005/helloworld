89efdbca43560ceb3747cbeeff1000a02b8a2413
Mani Rathnam <mrathnam005@gmail.com>
2021-05-09 12:53:44 +0530

    added hello world app

---
+++ SpringbootDemo/src/main/java/com/coderulagam/demo/SpringbootDemoApplication.java
@@ -0,0 +1,17 @@
+package com.coderulagam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	
+		
+		System.out.println("Hello World ");
+	}
+
+	
+    }
